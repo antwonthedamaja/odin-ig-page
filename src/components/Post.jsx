f4ccdb03b6e84f12479bf9dc01391b1ab84984d6
antwonthedamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-05-31 18:49:54 -0400

    Change rendering to conditional from direct manipulation

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import '../style/Post.css';
 
 export default function Post() {
+    const [image, setImage] = useState(false);
+    const inputElement = useRef();
+
+    function clickInput() {
+        inputElement.current.click();
+    };
 
     function uploadImage(e) {
-        const output = document.querySelector('output');
-        output.textContent = '';
-        const image = document.querySelector('#preview');
-        image.src = URL.createObjectURL(e.target.files[0]);
-        const container = document.querySelector('#upload-image-container');
-        container.classList.remove('active');
+        if (e.target.files[0]) {
+            const newPicture = URL.createObjectURL(e.target.files[0]);
+            setImage(newPicture);
+        }
     }
 
-    return <main id='create-post-container'>
-        <div id='post-box' onClick={() => document.querySelector('#file').click()}>
-            <div id='upload-image-container' className='active'>
+    return !image ? <main id='create-post-container'>
+        <div id='post-box'>
+            <div id='upload-image-container' className='inactive' onClick={clickInput}>
                 <output>Add image to post</output>
-                <img id="preview" />
             </div>
-            <input onChange={uploadImage} id='file' type="file" accept="image/*" />
+            <input ref={inputElement} onChange={uploadImage} id='file' type="file" accept="image/jpg, image/jpeg, image/png, image/webp" />
+            <textarea id='post-text'></textarea>
+        </div>
+    </main> : <main id='create-post-container'>
+        <div id='post-box'>
+            <div id='upload-image-container' className='active' onClick={clickInput}>
+                <img id="preview" src={image} />
+            </div>
+            <input ref={inputElement} onChange={uploadImage} id='file' type="file" accept="image/jpg, image/jpeg, image/png, image/webp" />
+            <textarea id='post-text' maxLength='300'></textarea>
+            <button id='submit-post'>Submit?</button>
         </div>
     </main>;
 }
