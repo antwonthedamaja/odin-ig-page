246c7a26bc974077d3a48f664ba0cc8ae5856394
antwonthedamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-06-05 21:34:10 -0400

    Code image upload functionality

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,29 +1,57 @@
 import React, { useState, useRef } from 'react';
 import { auth } from '../firebaseConfig';
 import { updateProfile } from 'firebase/auth';
+import { storage } from '../firebaseConfig';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 
 export default function Post() {
     const [image, setImage] = useState(auth.currentUser.photoURL);
-    const inputElement = useRef();
+    const [type, setType] = useState('');
+    const [uploading, setUploading] = useState(false);
+    const [name, setName] = useState('');
+    const inputImage = useRef();
 
     function clickInput() {
-        inputElement.current.click();
+        inputImage.current.click();
     }
 
     function uploadImage(e) {
         if (e.target.files[0]) {
             const newPicture = URL.createObjectURL(e.target.files[0]);
             setImage(newPicture);
+            setType(e.target.files[0].type);
         }
     }
 
     async function updateAccount() {
-        try {
-            await updateProfile(auth.currentUser, {
-                displayName: document.querySelector('#newname').value || auth.currentUser.displayName, photoURL: image
-            });
-        } catch (err) {
-            return console.error(err);
+        if (image != auth.currentUser.photoURL) {
+            setUploading(true);
+            try {
+                const response = await fetch(image);
+                const data = await response.blob();
+                const pfpRef = ref(storage, `images/pfps/${auth.currentUser.uid}`);
+                const metadata = { contentType: type };
+                const pfpUpload = await uploadBytes(pfpRef, data, metadata);
+                const newPfp = await getDownloadURL(pfpUpload.ref);
+                await updateProfile(auth.currentUser, {
+                    displayName: name || auth.currentUser.displayName, photoURL: newPfp
+                });
+            } catch (err) {
+                return console.error(err);
+            }
+            alert('success');
+            setUploading(false);
+        } else {
+            setUploading(true);
+            try {
+                await updateProfile(auth.currentUser, {
+                    displayName: name || auth.currentUser.displayName, photoURL: image
+                });
+            } catch (err) {
+                console.error(err);
+            }
+            alert('name changed');
+            setUploading(false);
         }
     }
 
@@ -32,17 +60,18 @@ export default function Post() {
             <div id="settings-container">
                 <div id="pfp-container">
                     <b>Change picture:</b>
-                    <img id="pfp" src={image} onClick={clickInput}/>
-                    <input ref={inputElement} onChange={uploadImage} style={{display: 'none'}} 
+                    <img id="pfp" src={image} onClick={clickInput} />
+                    <input ref={inputImage} onChange={uploadImage} style={{display: 'none'}} 
                     type="file" accept="image/jpg, image/jpeg, image/png, image/webp" />
                 </div>
                 <div>
                     <span><b>Current display name:</b> {auth.currentUser.displayName}</span>
-                    <input type='text' placeholder="New display name" id="newname" maxLength="20" />
+                    <input type='text' placeholder="New display name" maxLength="20" 
+                    onChange={(e) => setName(e.target.value)}/>
                 </div>
             </div>
             <div id="update-container">
-                <button id="update" onClick={updateAccount}>Update profile</button>
+                <button id="update" disabled={uploading} onClick={updateAccount}>Update profile</button>
             </div>
         </div>
     </main>;
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBKrAGVB5qfsDsfeTQJ2nR-J7JWtXDqOrs',
@@ -7,9 +8,13 @@ const firebaseConfig = {
   projectId: 'odin-ig-page',
   storageBucket: 'odin-ig-page.appspot.com',
   messagingSenderId: '740578172593',
-  appId: '1:740578172593:web:b80f5482e164da6e0a0c90'
+  appId: '1:740578172593:web:b80f5482e164da6e0a0c90',
+  storageBucket: 'gs://odin-ig-page.appspot.com'
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+export const storage = getStorage(app);
+// export const pfpRef = ref(storage, 'images/pfps');
+// export const postRef = ref(storage, 'images/posts');
